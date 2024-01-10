05f76415c0b0d913a74eb5ea3f1ceca2698fd04e
united2399 <115886821+united2399@users.noreply.github.com>
2024-01-10 11:01:43 +0100

    Add files via upload
    just importing everything

---
+++ main.py
@@ -0,0 +1,9 @@
+# Latest version. Defualt.
+try:
+    import ver_12
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+try:
+    input("")
+except Exception as e:
+    print(f"An unexpected error occured: {e}")
---
+++ readme.txt
@@ -0,0 +1,5 @@
+if you cant run the game on replit and need to download it or smth:
+you only need to get these:
+main.py
+background.png
+ver_12.py
