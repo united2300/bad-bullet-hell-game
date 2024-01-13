10ce62008b3ced2afd4be958e2e6f351bb54aa52
united2399 <115886821+united2399@users.noreply.github.com>
2024-01-13 17:59:06 +0100

    Update main.py
    input functionality..?

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # Latest version. Defualt.
 try:
-    import ver_12
+    while True:
+        print ("Gamemode Selection:")
+        print ("(1) Classic")
+        print ("(2) Wavebased")
+        e = input("")
+        if e == "1":
+          import ver_12
+        elif e == "2":
+          import ver_12_wavebased
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
     try:
